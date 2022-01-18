7eaed473132a0473198b833fabbede76f2c8006b
coax1d <andrewburger1991@gmail.com>
2022-01-18 03:08:50 -0800

    impl fp_rpc::EthereumRuntimeRPCApi in wrong indentation block (#10)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -439,137 +439,6 @@ impl pallet_ethereum::Config for Runtime {
 	type StateRoot = pallet_ethereum::IntermediateStateRoot;
 }
 
-impl fp_rpc::EthereumRuntimeRPCApi for Runtime {
-	fn chain_id() -> u64 {
-		<Runtime as pallet_evm::Config>::ChainId::get()
-	}
-
-	fn account_basic(address: H160) -> EVMAccount {
-		EVM::account_basic(&address)
-	}
-
-	fn gas_price() -> U256 {
-		<Runtime as pallet_evm::Config>::FeeCalculator::min_gas_price()
-	}
-
-	fn account_code_at(address: H160) -> Vec<u8> {
-		EVM::account_codes(address)
-	}
-
-	fn author() -> H160 {
-		<pallet_evm::Pallet<Runtime>>::find_author()
-	}
-
-	fn storage_at(address: H160, index: U256) -> H256 {
-		let mut tmp = [0u8; 32];
-		index.to_big_endian(&mut tmp);
-		EVM::account_storages(address, H256::from_slice(&tmp[..]))
-	}
-
-	fn call(
-		from: H160,
-		to: H160,
-		data: Vec<u8>,
-		value: U256,
-		gas_limit: U256,
-		max_fee_per_gas: Option<U256>,
-		max_priority_fee_per_gas: Option<U256>,
-		nonce: Option<U256>,
-		estimate: bool,
-		access_list: Option<Vec<(H160, Vec<H256>)>>,
-	) -> Result<pallet_evm::CallInfo, sp_runtime::DispatchError> {
-		let config = if estimate {
-			let mut config = <Runtime as pallet_evm::Config>::config().clone();
-			config.estimate = true;
-			Some(config)
-		} else {
-			None
-		};
-
-		<Runtime as pallet_evm::Config>::Runner::call(
-			from,
-			to,
-			data,
-			value,
-			gas_limit.low_u64(),
-			max_fee_per_gas,
-			max_priority_fee_per_gas,
-			nonce,
-			Vec::new(),
-			config.as_ref().unwrap_or(<Runtime as pallet_evm::Config>::config()),
-		).map_err(|err| err.into())
-	}
-
-	fn create(
-		from: H160,
-		data: Vec<u8>,
-		value: U256,
-		gas_limit: U256,
-		max_fee_per_gas: Option<U256>,
-		max_priority_fee_per_gas: Option<U256>,
-		nonce: Option<U256>,
-		estimate: bool,
-		access_list: Option<Vec<(H160, Vec<H256>)>>,
-	) -> Result<pallet_evm::CreateInfo, sp_runtime::DispatchError> {
-		let config = if estimate {
-			let mut config = <Runtime as pallet_evm::Config>::config().clone();
-			config.estimate = true;
-			Some(config)
-		} else {
-			None
-		};
-
-		<Runtime as pallet_evm::Config>::Runner::create(
-			from,
-			data,
-			value,
-			gas_limit.low_u64(),
-			max_fee_per_gas,
-			max_priority_fee_per_gas,
-			nonce,
-			Vec::new(),
-			config.as_ref().unwrap_or(<Runtime as pallet_evm::Config>::config()),
-		).map_err(|err| err.into())
-	}
-
-	fn current_transaction_statuses() -> Option<Vec<TransactionStatus>> {
-		Ethereum::current_transaction_statuses()
-	}
-
-	fn current_block() -> Option<pallet_ethereum::Block> {
-		Ethereum::current_block()
-	}
-
-	fn current_receipts() -> Option<Vec<pallet_ethereum::Receipt>> {
-		Ethereum::current_receipts()
-	}
-
-	fn current_all() -> (
-		Option<pallet_ethereum::Block>,
-		Option<Vec<pallet_ethereum::Receipt>>,
-		Option<Vec<TransactionStatus>>
-	) {
-		(
-			Ethereum::current_block(),
-			Ethereum::current_receipts(),
-			Ethereum::current_transaction_statuses()
-		)
-	}
-
-	fn extrinsic_filter(
-		xts: Vec<<Block as BlockT>::Extrinsic>,
-	) -> Vec<EthereumTransaction> {
-		xts.into_iter().filter_map(|xt| match xt.0.function {
-			Call::Ethereum(transact{transaction}) => Some(transaction),
-			_ => None
-		}).collect::<Vec<EthereumTransaction>>()
-	}
-
-	fn elasticity() -> Option<Permill> {
-		Some(Permill::zero()) // temp until paid parachain is functional
-	}
-}
-
 parameter_types! {
 	pub const RelayLocation: MultiLocation = MultiLocation::parent();
 	pub const RelayNetwork: NetworkId = NetworkId::Any;
@@ -970,6 +839,137 @@ impl_runtime_apis! {
 	impl cumulus_primitives_core::CollectCollationInfo<Block> for Runtime {
 		fn collect_collation_info() -> cumulus_primitives_core::CollationInfo {
 			ParachainSystem::collect_collation_info()
+		}
+	}
+
+	impl fp_rpc::EthereumRuntimeRPCApi<Block> for Runtime {
+		fn chain_id() -> u64 {
+			<Runtime as pallet_evm::Config>::ChainId::get()
+		}
+
+		fn account_basic(address: H160) -> EVMAccount {
+			EVM::account_basic(&address)
+		}
+
+		fn gas_price() -> U256 {
+			<Runtime as pallet_evm::Config>::FeeCalculator::min_gas_price()
+		}
+
+		fn account_code_at(address: H160) -> Vec<u8> {
+			EVM::account_codes(address)
+		}
+
+		fn author() -> H160 {
+			<pallet_evm::Pallet<Runtime>>::find_author()
+		}
+
+		fn storage_at(address: H160, index: U256) -> H256 {
+			let mut tmp = [0u8; 32];
+			index.to_big_endian(&mut tmp);
+			EVM::account_storages(address, H256::from_slice(&tmp[..]))
+		}
+
+		fn call(
+			from: H160,
+			to: H160,
+			data: Vec<u8>,
+			value: U256,
+			gas_limit: U256,
+			max_fee_per_gas: Option<U256>,
+			max_priority_fee_per_gas: Option<U256>,
+			nonce: Option<U256>,
+			estimate: bool,
+			access_list: Option<Vec<(H160, Vec<H256>)>>,
+		) -> Result<pallet_evm::CallInfo, sp_runtime::DispatchError> {
+			let config = if estimate {
+				let mut config = <Runtime as pallet_evm::Config>::config().clone();
+				config.estimate = true;
+				Some(config)
+			} else {
+				None
+			};
+
+			<Runtime as pallet_evm::Config>::Runner::call(
+				from,
+				to,
+				data,
+				value,
+				gas_limit.low_u64(),
+				max_fee_per_gas,
+				max_priority_fee_per_gas,
+				nonce,
+				access_list.unwrap_or_default(),
+				config.as_ref().unwrap_or(<Runtime as pallet_evm::Config>::config()),
+			).map_err(|err| err.into())
+		}
+
+		fn create(
+			from: H160,
+			data: Vec<u8>,
+			value: U256,
+			gas_limit: U256,
+			max_fee_per_gas: Option<U256>,
+			max_priority_fee_per_gas: Option<U256>,
+			nonce: Option<U256>,
+			estimate: bool,
+			access_list: Option<Vec<(H160, Vec<H256>)>>,
+		) -> Result<pallet_evm::CreateInfo, sp_runtime::DispatchError> {
+			let config = if estimate {
+				let mut config = <Runtime as pallet_evm::Config>::config().clone();
+				config.estimate = true;
+				Some(config)
+			} else {
+				None
+			};
+
+			<Runtime as pallet_evm::Config>::Runner::create(
+				from,
+				data,
+				value,
+				gas_limit.low_u64(),
+				max_fee_per_gas,
+				max_priority_fee_per_gas,
+				nonce,
+				access_list.unwrap_or_default(),
+				config.as_ref().unwrap_or(<Runtime as pallet_evm::Config>::config()),
+			).map_err(|err| err.into())
+		}
+
+		fn current_transaction_statuses() -> Option<Vec<TransactionStatus>> {
+			Ethereum::current_transaction_statuses()
+		}
+
+		fn current_block() -> Option<pallet_ethereum::Block> {
+			Ethereum::current_block()
+		}
+
+		fn current_receipts() -> Option<Vec<pallet_ethereum::Receipt>> {
+			Ethereum::current_receipts()
+		}
+
+		fn current_all() -> (
+			Option<pallet_ethereum::Block>,
+			Option<Vec<pallet_ethereum::Receipt>>,
+			Option<Vec<TransactionStatus>>
+		) {
+			(
+				Ethereum::current_block(),
+				Ethereum::current_receipts(),
+				Ethereum::current_transaction_statuses()
+			)
+		}
+
+		fn extrinsic_filter(
+			xts: Vec<<Block as BlockT>::Extrinsic>,
+		) -> Vec<EthereumTransaction> {
+			xts.into_iter().filter_map(|xt| match xt.0.function {
+				Call::Ethereum(transact{transaction}) => Some(transaction),
+				_ => None
+			}).collect::<Vec<EthereumTransaction>>()
+		}
+
+		fn elasticity() -> Option<Permill> {
+			Some(Permill::zero()) // temp until paid parachain is functional
 		}
 	}
 
